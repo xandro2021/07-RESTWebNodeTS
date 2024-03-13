a8be0a3e07914a2915fc4d43f1318e47139ed140
xandro2021 <xandrocastro2021@gmail.com>
2024-03-12 20:22:39 -0600

    introduccion arquitectura limpia + notas

---
+++ src/domain/entities/todo.entity.ts
@@ -0,0 +1,13 @@
+export class TodoEntity {
+
+  constructor(
+    public id: number,
+    public text: string,
+    public completedAt?: Date | null
+  ) { }
+
+  get isCompleted() {
+    return !!this.completedAt;
+  }
+
+}
